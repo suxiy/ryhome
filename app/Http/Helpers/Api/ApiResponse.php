946cf0d272425e8b7e7a3653f13fb34e13defdc5
steven <1>
2019-12-18 11:05:54 +0800

    1

--- app/Http/Helpers/Api/ApiResponse.php
+++ app/Http/Helpers/Api/ApiResponse.php
@@ -15,7 +15,11 @@ trait ApiResponse
     public function respond($data){
         //返回时统一记录接口入参出参日志
         $requestUri = explode('/',request()->getRequestUri());
-        $method = end($requestUri);
+        $dir = 'api';
+        if(in_array('spi',$requestUri)){
+            $dir = 'spi';
+        }
+        $method = implode('-',array_slice($requestUri,array_search($dir,$requestUri)+1,count($requestUri)-1));
         $strpos = strripos($method,"?");
         if($strpos!==false){
             $method = substr($method,0,$strpos);
@@ -24,22 +28,32 @@ trait ApiResponse
         $RespondData = $data;
         $log = compact('RequestData','RespondData');
         //系统异常记录到日志 不需要显示给前台
-        $log['error']=isset($this->error)?$this->error:'';
-        log_json('api',$method,json_encode($log,JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES));
+        $log['error']=$this->error??'';
+        log_array($dir,$method,$log);
         return response()->json($data);
     }
 
     public function success($data=[], $msg = '成功'){
-        return $this->respond(['code'=>1,'msg'=>$msg,'data'=>$data]);
+        return $this->respond(['code'=>1,'message'=>$msg,'data'=>$data]);
     }
 
-    public function error($msg = '网络异常'){
-        if($msg instanceof \Exception || $msg instanceof QueryException){
-            $this->error = $msg->getMessage();
-            //code非1 属于系统抛出的异常 将此异常放入日志 不需要给前台
-            $msg = (config('app.name')||$msg->getCode()==1)?$msg->getMessage():'网络异常';
+    /**
+     * 致命错误
+     * @param $exception
+     * @param array $data(强制返回的格式)
+     * @return mixed
+     */
+    public function error($exception,$data = []){
+        if($exception instanceof \Exception || $exception instanceof QueryException){
+            $this->error = $exception->getMessage().'--file:'.$exception->getFile().'--line:'.$exception->getLine();
         }
-        return $this->respond(['code'=>0,'msg'=>$msg]);
+        if($exception->getCode()==1){
+            $msg = $exception->getMessage();
+        }else{
+            $msg = config('app.debug')?$exception->getMessage():'网络异常';
+        }
+        $data = $data?:['code'=>0,'message'=>$msg];
+        return $this->respond($data);
     }
 
     /**
@@ -47,8 +61,72 @@ trait ApiResponse
      * @param string $msg
      * @return mixed
      */
-    public function fail($msg = '失败'){
-        return $this->respond(['code'=>0,'msg'=>$msg]);
+    public function fail($msg = '网络异常'){
+        return $this->respond(['code'=>0,'message'=>$msg]);
     }
 
+    /**
+     * 跳注册页面
+     * @return mixed
+     */
+    public function failLogin(){
+        return $this->respond([
+            'code'=>2,
+            'message'=>'',
+            'data'=>[
+                'isConfirm'=>false,
+                'name'=>'',
+                'phone'=>''
+            ]
+        ]);
+    }
+
+    /**
+     * 跳指定地址
+     * @param $url
+     * @return mixed
+     */
+    public function redirectLogin($url){
+        return $this->respond([
+            'code'=>3,
+            'message'=>'',
+            'data'=>[
+                'isConfirm'=>false,
+                'name'=>'',
+                'phone'=>'',
+                'loginUrl'=>$url,
+            ]
+        ]);
+    }
+
+    /**
+     * 弹框显示获赠优惠券
+     * @param $coupon
+     * @return mixed
+     */
+    public function receivedCoupon($coupon){
+        return $this->respond([
+            'code'=>4,
+            'message'=>'',
+            'data'=>[
+                'isConfirm'=>false,
+                'name'=>'',
+                'phone'=>'',
+                'coupon'=>$coupon,
+            ]
+        ]);
+    }
+
+    /**
+     * 弹框提示
+     * @param $msg
+     * @return mixed
+     */
+    public function alert($msg){
+        return $this->respond([
+            'code'=>5,
+            'message'=>$msg,
+            'data'=>[]
+        ]);
+    }
 }
