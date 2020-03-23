8dd32d541e101d1c12c87abf01e116f4dd71f042
steven <1>
2020-03-23 08:40:48 +0800

    1

--- app/Lib/WechatService.php
+++ app/Lib/WechatService.php
@@ -24,6 +24,7 @@ class WechatService
             ],
         ];
         $this->app = Factory::officialAccount($config);
+        $this->app['cache'] = new \Overtrue\LaravelWeChat\CacheBridge(app('cache.store'));
     }
 
     public function serve(){
